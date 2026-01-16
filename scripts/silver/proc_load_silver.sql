74011b81fbd7dd27178320867bda438fb33b5952
Mr. Dara <129968535+Dara-Mapping@users.noreply.github.com>
2026-01-16 12:05:44 +0700

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -32,52 +32,52 @@ BEGIN
 		PRINT 'Loading CRM Tables';
 		PRINT '------------------------------------------';
 
-		-- Loading silver.silver.crm_cust_info
-		SET @start_time = GETDATE();
-		PRINT '>> Truncate Table: silver.crm_cust_info';
+			-- Loading silver.crm_cust_info
+        SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.crm_cust_info';
 		TRUNCATE TABLE silver.crm_cust_info;
 		PRINT '>> Inserting Data Into: silver.crm_cust_info';
 		INSERT INTO silver.crm_cust_info (
-			cst_id,
-			cst_key,
-			cst_firstname,
-			cst_lastname,
-			cst_marital_status,
+			cst_id, 
+			cst_key, 
+			cst_firstname, 
+			cst_lastname, 
+			cst_marital_status, 
 			cst_gndr,
 			cst_create_date
-		) 
-
+		)
 		SELECT
 			cst_id,
 			cst_key,
 			TRIM(cst_firstname) AS cst_firstname,
 			TRIM(cst_lastname) AS cst_lastname,
-			CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
-				 WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
-				 ELSE 'n/a'
-			END cst_marital_status,
-			CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
-				 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
-				 ELSE 'n/a'
-			END AS cst_gndr,
-
+			CASE 
+				WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+				WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+				ELSE 'n/a'
+			END AS cst_marital_status, -- Normalize marital status values to readable format
+			CASE 
+				WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+				WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+				ELSE 'n/a'
+			END AS cst_gndr, -- Normalize gender values to readable format
 			cst_create_date
 		FROM (
 			SELECT
 				*,
-				ROW_NUMBER () OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
+				ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
 			FROM bronze.crm_cust_info
 			WHERE cst_id IS NOT NULL
-		) t 
-		WHERE flag_last = 1
+		) t
+		WHERE flag_last = 1; -- Select the most recent record per customer
 		SET @end_time = GETDATE();
-		PRINT '>> Loading Duration: ' + CAST (DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-		PRINT '>> ------------------------------------------';
+        PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> ------------------------------------------'
 
-		-- Loading silver.crm_cust_info
+		-- Loading silver.crm_prd_info
 		SET @start_time = GETDATE()
 		PRINT '>> Truncate Table: silver.crm_prd_info';
-		TRUNCATE TABLE silver.crm_cust_info;
+		TRUNCATE TABLE silver.crm_prd_info;
 		PRINT '>> Inserting Data Into: silver.crm_prd_info';
 		INSERT INTO silver.crm_prd_info (
 			prd_id,
@@ -112,7 +112,7 @@ BEGIN
 		-- Loading silver.crm_sales_details
 		SET @start_time = GETDATE();
 		PRINT '>> Truncate Table: silver.crm_sales_details';
-		TRUNCATE TABLE silver.crm_cust_info;
+		TRUNCATE TABLE silver.crm_sales_details;
 		PRINT '>> Inserting Data Into: silver.crm_sales_details';
 		INSERT INTO silver.crm_sales_details (
 			sls_ord_num,
@@ -159,7 +159,7 @@ BEGIN
 		-- Loading silver.erp_cust_az12
 		SET @start_time = GETDATE()
 		PRINT '>> Truncate Table: silver.erp_cust_az12';
-		TRUNCATE TABLE silver.crm_cust_info;
+		TRUNCATE TABLE  silver.erp_cust_az12;
 		PRINT '>> Inserting Data Into: silver.erp_cust_az12';
 		INSERT INTO silver.erp_cust_az12 (cid, bdate, gen)
 		SELECT 
@@ -182,7 +182,7 @@ BEGIN
 		-- Loading silver.erp_loc_a101
 		SET @start_time = GETDATE()
 		PRINT '>> Truncate Table: silver.erp_loc_a101';
-		TRUNCATE TABLE silver.crm_cust_info;
+		TRUNCATE TABLE silver.erp_loc_a101;
 		PRINT '>> Inserting Data Into: silver.erp_loc_a101';
 		INSERT INTO silver.erp_loc_a101 (cid, cntry)
 		SELECT
@@ -199,7 +199,7 @@ BEGIN
 		-- Loading silver.erp_px_cat_g1v2
 		SET @start_time = GETDATE()
 		PRINT '>> Truncate Table: silver.erp_px_cat_g1v2';
-		TRUNCATE TABLE silver.crm_cust_info;
+		TRUNCATE TABLE ilver.erp_px_cat_g1v2;
 		PRINT '>> Inserting Data Into: silver.erp_px_cat_g1v2';
 		INSERT INTO silver.erp_px_cat_g1v2
 			(id, cat, subcat, maintenance)
